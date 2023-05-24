1413e4ebc93c8b6c2636ff54469c2939a6bfc557
Andrei Prokudin <andrewicehab@gmail.com>
2023-05-24 14:39:57 +0700

    Modified app to count post likes and post sharings.

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,13 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    var likes: Int = 0,
+    var likedByMe: Boolean = false,
+    var shared: Int = 0,
+    var views: Int = 0,
+    var sharedByMe: Boolean = false,
+)
