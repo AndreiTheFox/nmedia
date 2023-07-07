d52157f63a3f7e240fa21dc20137d2425f4fcf3a
AndreiTheFox <andrewicehab@gmail.com>
2023-07-07 21:49:49 +0700

    Notifications with new POST and direct message

--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -19,6 +19,7 @@ import kotlin.random.Random
 
 class FCMService : FirebaseMessagingService() {
     private val channelId: String = "Nmedia Notifications"
+
     override fun onCreate() {
         super.onCreate()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -35,19 +36,14 @@ class FCMService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         when (message.data["action"]) {
             "LIKE" -> handleLike(Gson().fromJson(message.data["content"], Like::class.java))
-            "Share"-> "bla bla"//TODO
+            "NEW_MESSAGE"-> handleNewMessage(Gson().fromJson(message.data["content"], NewMessage::class.java))
+            "NEW_POST"->handleNewPost(Gson().fromJson(message.data["content"], NewPost::class.java))
             else -> return
         }
 
     }
 
     private fun handleLike(like: Like) {
-        val notifyPendingIntent = PendingIntent.getActivity(
-            this, 0,
-            Intent(this, AppActivity::class.java),
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-        )
-        println(like)
         val notificationMessage =
             getString(R.string.notification_user_liked, like.userName, like.postAuthor)
         val notification = NotificationCompat.Builder(this, channelId)
@@ -55,7 +51,7 @@ class FCMService : FirebaseMessagingService() {
             .setContentTitle(
                 notificationMessage
             )
-            .setContentIntent(notifyPendingIntent)
+            .setContentIntent(getCurrentPendingIntent())
             .setAutoCancel(true)
             .build()
 
@@ -69,20 +65,81 @@ class FCMService : FirebaseMessagingService() {
         }
     }
 
+    private fun handleNewMessage (newMessage: NewMessage){
+        val notificationMessage =
+            getString(R.string.notification_user_new_message, newMessage.senderName, newMessage.postContent)
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification_icon)
+            .setContentTitle (newMessage.senderName)
+            .setContentText(
+                notificationMessage
+            )
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText(notificationMessage))
+            .setContentIntent(getCurrentPendingIntent())
+            .setAutoCancel(true)
+            .build()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(this)
+                .notify(Random.nextInt(100_000), notification)
+        }
+    }
+    private fun handleNewPost (newPost: NewPost){
+        val notificationMessage =
+            getString(R.string.notification_user_new_post, newPost.authorName, newPost.postContent)
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification_icon)
+            .setContentTitle (newPost.authorName)
+            .setContentText(
+                notificationMessage
+            )
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText(notificationMessage))
+            .setContentIntent(getCurrentPendingIntent())
+            .setAutoCancel(true)
+            .build()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(this)
+                .notify(Random.nextInt(100_000), notification)
+        }
+    }
     override fun onNewToken(token: String) {
         println(token)
     }
+    private fun getCurrentPendingIntent(): PendingIntent{
 
-
-//    enum class Action {
-//        LIKE
-//        //      SHARE
-//    }
+        val notifyPendingIntent = PendingIntent.getActivity(
+            this, 0,
+            Intent(this, AppActivity::class.java),
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        return notifyPendingIntent
+    }
 
     data class Like(
         val userId: Long,
         val userName: String,
         val postId: Long,
         val postAuthor: String,
+    )
+    data class NewMessage (
+        val senderId: Long,
+        val senderName: String,
+        val postId: Long,
+        val postContent: String,
+            )
+    data class NewPost (
+        val authorId: Long,
+        val authorName: String,
+        val postId: Long,
+        val postContent: String,
     )
 }
